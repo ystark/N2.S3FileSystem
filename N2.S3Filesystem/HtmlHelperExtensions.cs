9f14c0a24251e180ed84f8e20c45384f2a60e303
Andrew Parsons <abparsons@gmail.com>
2013-02-14 13:07:34 -0600

    added n2image helper

---
+++ N2.S3Filesystem/HtmlHelperExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace N2.S3FileSystem {
+	using System.Web.Mvc;
+
+	public static class HtmlHelperExtensions {
+
+		public static string N2Image(this HtmlHelper helper, string virtualPath) {
+			if (virtualPath.StartsWith("http")) return virtualPath;
+			return string.IsNullOrWhiteSpace(virtualPath) ? null : string.Format("//s3.amazonaws.com/{0}/{1}", ConfigurationManager.AppSettings["AWSBucketName"], virtualPath.TrimStart('/'));
+		}
+
+	}
+}
