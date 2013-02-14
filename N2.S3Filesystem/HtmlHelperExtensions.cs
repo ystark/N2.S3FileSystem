877bd37cce1ec3040f5a839a63fd4114f01d10d5
Dave Cowart <davecowart@gmail.com>
2013-02-14 13:16:42 -0600

    fixed system.web.mvc using location, fixed config transform

--- N2.S3Filesystem/HtmlHelperExtensions.cs
+++ N2.S3Filesystem/HtmlHelperExtensions.cs
@@ -1,14 +1,11 @@
 ﻿using System.Configuration;
+using System.Web.Mvc;
 
 namespace N2.S3FileSystem {
-	using System.Web.Mvc;
-
 	public static class HtmlHelperExtensions {
-
 		public static string N2Image(this HtmlHelper helper, string virtualPath) {
 			if (virtualPath.StartsWith("http")) return virtualPath;
 			return string.IsNullOrWhiteSpace(virtualPath) ? null : string.Format("//s3.amazonaws.com/{0}/{1}", ConfigurationManager.AppSettings["AWSBucketName"], virtualPath.TrimStart('/'));
 		}
-
 	}
 }
